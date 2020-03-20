53c69b391735491ece3636ce4de05e16ffca2a39
Marcos Motta <mrcsmotta1@gmail.com>
2020-03-20 15:33:31 -0300

    Criando model Product

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+//iniciando o app
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello word');
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true
+});
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
+
+//Primeira rota
+app.get("/", (req, res) => {
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "http://github.com/facebook/react-native"
+  });
+
+  return res.send("hello word!");
 });
 
 app.listen(3001);
