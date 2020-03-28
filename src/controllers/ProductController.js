f7fefc4e343b0ff4838b7fdecf533848977889ea
Marcos Motta <mrcsmotta1@gmail.com>
2020-03-28 00:16:50 -0300

    Criado Swagger bda busca de produto por id

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -23,6 +23,57 @@ module.exports = {
     return res.json(products);
   },
 
+  /**
+   * @swagger
+   * /api/v1/products/{id}:
+   *  get:
+   *    description: Selecione um produto pelo id
+   *    tags:
+   *      - Products
+   *    consumes:
+   *      - application/json
+   *    produces:
+   *      - application/json
+   *    parameters:
+   *      - in: path
+   *        name: id
+   *        format: string
+   *        schema:
+   *          type: string
+   *        required: true
+   *        description: String id para selecionar Produtos
+   *    responses:
+   *       200:
+   *         description:
+   *         schema:
+   *          $ref: '#/definitions/Product'
+   *       400:
+   *          description: "Invalid ID supplied"
+   *       404:
+   *          description: "Order not found"
+   * definitions:
+   *    Product:
+   *       type: object
+   *       properties:
+   *         id:
+   *            type: string
+   *            example: "5e7d17bc9eb75d446aa078d5"
+   *         title:
+   *            type: string
+   *            example: "Api nodeJS"
+   *         description:
+   *            type: string
+   *            example: "Documentação Swagger"
+   *         url:
+   *            type: string
+   *            example: "https://swagger.io/"
+   *         createdAd:
+   *            type: string
+   *            format: date-time
+   *       xml:
+   *          name: 'Product'
+   *
+   */
   async show(req, res) {
     const product = await Product.findById(req.params.id);
 
